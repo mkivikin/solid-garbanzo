33775b55d5c9a0dbdb19a7a2ef3276850bda3c50
mkivikin <mkivikin@users.noreply.github.com>
2018-06-14 15:12:45 +0300

    mapScript.js return

--- mapScript.js
+++ mapScript.js
@@ -6,25 +6,18 @@ function initMap(){
       maxZoom: 19
     }
     var map = new google.maps.Map(document.getElementById('map'), options);
-    fetchMarkers();
-
+    var action = "default";
+    var value = "";
+    fetchMarkers(action, value);
 
     //=================fetchMarkers Start============================
-    function fetchMarkers() {
+    function fetchMarkers(action, value) {
       var xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var markers = JSON.parse(this.responseText);
-            var heatmapData = new Array();
-            for(m in markers) {
-                pos = new google.maps.LatLng(markers[m][1], markers[m][2]);
-                var weightedPos = {location: pos, weight: (markers[m][3]*10)};
-                heatmapData.push(weightedPos);
-            }
-              var heatmap = new google.maps.visualization.HeatmapLayer({
-              data: heatmapData,
-              map: map
-              });
+            console.log(markers);
+            createHeatmap(markers, map);
 
               /*google.maps.event.addListener(map, 'zoom_changed', function(){
                   var zoom = map.getZoom();
@@ -48,8 +41,34 @@ function initMap(){
       /*$.post("mapFunctions.php", {action:"test"}, function(result){
             var markers = result;
         });*/
-      xmlhttp.open("GET", "mapFunctions.php", true);
-      xmlhttp.send();
+      xmlhttp.open("POST", "mapFunctions.php", true);
+      xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      if(action === "default") {
+        xmlhttp.send("action=" + action);
+      }
     }
     //==========================fetchMarkers End========================
+}
+
+function createHeatmap(markers, map) {
+    var heatmapData = new Array();
+    for(m in markers) {
+        pos = new google.maps.LatLng(markers[m][1], markers[m][2]);
+        var weightedPos = {location: pos, weight: (markers[m][3]*10)};
+        heatmapData.push(weightedPos);
+    }
+      var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatmapData,
+      map: map
+    });
+}
+
+
+
+function checkBox(id){
+  let checkBox = document.getElementById(id);
+  if(checkBox.checked){
+    fetchMarkers("byExperimentID", id);
+    console.log("Ahoy");
+  }
 }
